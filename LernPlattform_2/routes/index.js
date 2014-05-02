f64e7d93c8d516f0d51999ca68312a4de2031aef
Honnigorega <Hendrik@Hendriks-MacBook-Pro-2.local>
2014-05-02 20:32:13 +0200

    plattform_2

---
+++ LernPlattform_2/routes/index.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.render('home', { title: 'Home' });
+});
+
+/* GET Module page. */
+router.get('/module', function(req, res) {
+    res.render('module', { title: 'Module' });
+});
+
+/* GET WBA 2 page. */
+router.get('/wba2', function(req, res) {
+	res.render('wba2', { title: 'WBA 2' })
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Melde dich an' });
+});
+
+/* GET Kommentarliste. */
+router.get('/material1', function(req, res){
+    var db = req.db;
+    var collection = db.get('kommentarcollection');
+    collection.find({},{},function(e,docs){
+        res.render('material1', {
+            "material1" : docs
+        });
+    });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("module");
+            // And forward to success page
+            res.redirect("module");
+        }
+    });
+});
+
+/* POST to ADD Kommentar */
+router.post('/addkommentar', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var kommentar = req.body.kommentar;
+
+    // Set our collection
+    var collection = db.get('kommentarcollection');
+
+    // Submit to the DB
+    collection.insert({
+        "kommentar" : kommentar
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /addkommentar
+            res.location("material1");
+            // And forward to success page
+            res.redirect("material1");
+        }
+    });
+});
+
+
+module.exports = router;
